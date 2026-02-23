5503410f7c0e3376ecac8263c015d1d03cda76a9
Mahriar Gharaghani <166933043+UDynamic@users.noreply.github.com>
2026-02-23 20:54:31 +0330

    train and it's helper is completed

---
+++ projects/02-deep-learning-cifar/src/train.py
@@ -0,0 +1,85 @@
+# train.py
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+from src.model import SimpleCNN
+from src.dataset import get_data_loaders  # we'll create this function
+from src.utils import accuracy, save_checkpoint
+
+def train_one_epoch(model, loader, criterion, optimizer, device):
+    model.train()
+    running_loss = 0.0
+    running_acc = 0.0
+    for images, labels in loader:
+        images, labels = images.to(device), labels.to(device)
+        
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item() * images.size(0)
+        running_acc += accuracy(outputs, labels) * images.size(0)
+    
+    epoch_loss = running_loss / len(loader.dataset)
+    epoch_acc = running_acc / len(loader.dataset)
+    return epoch_loss, epoch_acc
+
+def validate(model, loader, criterion, device):
+    model.eval()
+    running_loss = 0.0
+    running_acc = 0.0
+    with torch.no_grad():
+        for images, labels in loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            running_loss += loss.item() * images.size(0)
+            running_acc += accuracy(outputs, labels) * images.size(0)
+    
+    epoch_loss = running_loss / len(loader.dataset)
+    epoch_acc = running_acc / len(loader.dataset)
+    return epoch_loss, epoch_acc
+
+def main():
+    # Hyperparameters (could be loaded from config)
+    num_epochs = 20
+    batch_size = 64
+    learning_rate = 0.001
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    # Data
+    train_loader, test_loader = get_data_loaders(batch_size=batch_size)
+    
+    # Model, criterion, optimizer
+    model = SimpleCNN(num_classes=10).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    
+    # TensorBoard writer
+    writer = SummaryWriter('runs/cifar10_experiment')
+    
+    for epoch in range(num_epochs):
+        train_loss, train_acc = train_one_epoch(model, train_loader, criterion, optimizer, device)
+        val_loss, val_acc = validate(model, test_loader, criterion, device)
+        
+        # Log to TensorBoard
+        writer.add_scalar('Loss/train', train_loss, epoch)
+        writer.add_scalar('Loss/val', val_loss, epoch)
+        writer.add_scalar('Accuracy/train', train_acc, epoch)
+        writer.add_scalar('Accuracy/val', val_acc, epoch)
+        
+        print(f"Epoch {epoch+1}/{num_epochs} | Train Loss: {train_loss:.4f} Acc: {train_acc:.4f} | Val Loss: {val_loss:.4f} Acc: {val_acc:.4f}")
+        
+        # Save checkpoint every 5 epochs
+        if (epoch+1) % 5 == 0:
+            save_checkpoint(model, optimizer, epoch, f'models/checkpoint_epoch{epoch+1}.pth')
+    
+    writer.close()
+    # Save final model
+    torch.save(model.state_dict(), 'models/final_model.pth')
+
+if __name__ == '__main__':
+    main()
--- projects/02-deep-learning-cifar/src/utils.py
+++ projects/02-deep-learning-cifar/src/utils.py
@@ -7,3 +7,18 @@ test_dataset  = CIFAR10Dataset(root='./data', train=False, transform=test_transf
 
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=2)
 test_loader  = DataLoader(test_dataset, batch_size=64, shuffle=False, num_workers=2)
+
+# 
+import torch
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, 1)
+    return torch.sum(preds == labels).item() / len(labels)
+
+def save_checkpoint(model, optimizer, epoch, filename):
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+    }, filename)
+    print(f"Checkpoint saved to {filename}")
