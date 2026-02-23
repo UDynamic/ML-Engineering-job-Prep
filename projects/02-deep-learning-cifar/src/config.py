42e0fcf28f840649ad097b6ab65c155208471c92
Mahriar Gharaghani <166933043+UDynamic@users.noreply.github.com>
2026-02-23 20:58:25 +0330

    added config object into the config.py

--- projects/02-deep-learning-cifar/src/config.py
+++ projects/02-deep-learning-cifar/src/config.py
@@ -20,3 +20,17 @@ test_transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
 ])
+
+
+config = {
+    'model': 'resnet18',   # or 'simple_cnn'
+    'pretrained': True,
+    'batch_size': 64,
+    'epochs': 20,
+    'optimizer': 'adam',
+    'lr': 0.001,
+    'momentum': 0.9,
+    'weight_decay': 5e-4,
+    'augmentation': True,
+    'log_dir': 'runs/exp1'
+}
